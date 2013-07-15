7f1bd71f8a99565f98c7084596846fa05a533259
Ash <ash.eldritch@gmail.com>
2013-07-14 17:28:29 -0700

    trivial

--- handlers/signin.py
+++ handlers/signin.py
@@ -10,12 +10,11 @@ class Handler(webapp2.RequestHandler):
     @decorators.sends_response
     def get(self):
 
-        # Here we just delegate sign in to Google Accounts.
-        # If you want to handle it yourself, change here and in library.users module
-
         signedin_landing_url = self.request.get('origin') or routes.configs.get_default_redirect_after_signin().url
 
         if users.is_signed_in():
             self.redirect(signedin_landing_url)
         else:
+            # Here we just delegate sign in to Google Accounts.
+            # If you want to handle it yourself, change here and in library.users module
             self.redirect(users.create_google_signin_url(signedin_landing_url))
